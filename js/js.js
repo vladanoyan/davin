4fb2bca36c8a8402cdf3f1904dabda3cee37b699
Vlad <anoyanvlad@gmail.com>
2017-05-30 18:22:28 +0400

    first commit

--- js/js.js
+++ js/js.js
@@ -1,6 +1,12 @@
 
 
 $(document).ready(function(){
+
+    /** Refresh Modal Window Content */
+    $(document).on('hidden.bs.modal', function (e) {
+        $(e.target).removeData('bs.modal');
+    });
+
  /*__________Tooltip Initialization Starts__________*/
 var startTooltip= function() {
     var hideAllTooltips = function() {
@@ -40,6 +46,12 @@ $(document).ajaxComplete(function() {
     startTooltip();
 });
 /*__________Tooltip Initialization Ends__________*/
+
+
+/*__________file UPload text change start__________*/
+
+!function(a,b,c){var d=a.querySelectorAll(".fileUpload");Array.prototype.forEach.call(d,function(a){var b=a.nextElementSibling,c=b.innerHTML;a.addEventListener("change",function(a){var d="";d=this.files&&this.files.length>1?(this.getAttribute("data-multiple-caption")||"").replace("{count}",this.files.length):a.target.value.split("\\").pop(),d?b.querySelector("span").innerHTML=d:b.innerHTML=c}),a.addEventListener("focus",function(){a.classList.add("has-focus")}),a.addEventListener("blur",function(){a.classList.remove("has-focus")})})}(document,window,0);
+/*__________file UPload text change end__________*/
 
 
 /*_________Changing Toggle Button Active State Starts__________*/
@@ -200,8 +212,8 @@ $('#nav-icon3').click(function(){
 
     $('.ourSecondCarousel').owlCarousel({
 		margin: 20,
-		loop:true,
-		nav: true,
+    	nav: true,
+        loop:true,
         autoplay:true,
 		autoplayTimeout:3000,
 		navText: ["<",">"],
